a2087bcc23b6578268c03cd8bdb866aca13fc374
Upasana Kuar <upasanakuar44@gmail.com>
2022-02-11 22:07:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+document.getElementById("html").addEventListener("mouseover",function(){
+    document.getElementById("htmlhide").style.display="block";
+})
+document.getElementById("html").addEventListener("mouseout",function(){
+    document.getElementById("htmlhide").style.display="none";
+})
+
+document.getElementById("css").addEventListener("mouseover",function(){
+    document.getElementById("csshide").style.display="block";
+})
+document.getElementById("css").addEventListener("mouseout",function(){
+    document.getElementById("csshide").style.display="none";
+})
+
+document.getElementById("js").addEventListener("mouseover",function(){
+    document.getElementById("jshide").style.display="block";
+})
+document.getElementById("js").addEventListener("mouseout",function(){
+    document.getElementById("jshide").style.display="none";
+})
+
+document.getElementById("react").addEventListener("mouseover",function(){
+    document.getElementById("reacthide").style.display="block";
+})
+document.getElementById("react").addEventListener("mouseout",function(){
+    document.getElementById("reacthide").style.display="none";
+})
+
+document.getElementById("redux").addEventListener("mouseover",function(){
+    document.getElementById("reduxhide").style.display="block";
+})
+document.getElementById("redux").addEventListener("mouseout",function(){
+    document.getElementById("reduxhide").style.display="none";
+})
+
+
+document.getElementById("node").addEventListener("mouseover",function(){
+    document.getElementById("nodehide").style.display="block";
+})
+document.getElementById("node").addEventListener("mouseout",function(){
+    document.getElementById("nodehide").style.display="none";
+})
+
+document.getElementById("express").addEventListener("mouseover",function(){
+    document.getElementById("expresshide").style.display="block";
+})
+document.getElementById("express").addEventListener("mouseout",function(){
+    document.getElementById("expresshide").style.display="none";
+})
+
+document.getElementById("mongo").addEventListener("mouseover",function(){
+    document.getElementById("mongohide").style.display="block";
+})
+document.getElementById("mongo").addEventListener("mouseout",function(){
+    document.getElementById("mongohide").style.display="none";
+})
+
+document.getElementById("postman").addEventListener("mouseover",function(){
+    document.getElementById("postmanhide").style.display="block";
+})
+document.getElementById("postman").addEventListener("mouseout",function(){
+    document.getElementById("postmanhide").style.display="none";
+})
+
+
+document.getElementById("github").addEventListener("mouseover",function(){
+    document.getElementById("githubhide").style.display="block";
+})
+document.getElementById("github").addEventListener("mouseout",function(){
+    document.getElementById("githubhide").style.display="none";
+})
+document.getElementById("heroku").addEventListener("mouseover",function(){
+    document.getElementById("herokuhide").style.display="block";
+})
+document.getElementById("heroku").addEventListener("mouseout",function(){
+    document.getElementById("herokuhide").style.display="none";
+})
+document.getElementById("netlify").addEventListener("mouseover",function(){
+    document.getElementById("netlifyhide").style.display="block";
+})
+document.getElementById("netlify").addEventListener("mouseout",function(){
+    document.getElementById("netlifyhide").style.display="none";
+})
+
+document.getElementById("git").addEventListener("mouseover",function(){
+    document.getElementById("githide").style.display="block";
+    document.getElementById("githide").style.textAlign="center";
+})
+document.getElementById("git").addEventListener("mouseout",function(){
+    document.getElementById("githide").style.display="none";
+})
